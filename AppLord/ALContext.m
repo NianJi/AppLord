03c0d02f4744d64bc3a5565b833358247d1eb673
skyline75489 <skyline75489@outlook.com>
2016-03-21 09:37:28 +0800

    typo

--- AppLord/ALContext.m
+++ AppLord/ALContext.m
@@ -73,7 +73,7 @@ dispatch_semaphore_signal(_configLock);
 
 #pragma mark - service
 
-- (void)registService:(Protocol *)proto withImpl:(Class)implClass
+- (void)registerService:(Protocol *)proto withImpl:(Class)implClass
 {
     NSParameterAssert(proto != nil);
     NSParameterAssert(implClass != nil);
@@ -138,7 +138,7 @@ dispatch_semaphore_signal(_configLock);
 
 #pragma mark - module
 
-- (void)registModule:(Class)moduleClass
+- (void)registerModule:(Class)moduleClass
 {
     NSParameterAssert(moduleClass != nil);
     
